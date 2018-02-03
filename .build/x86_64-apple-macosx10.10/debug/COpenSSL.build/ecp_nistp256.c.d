ff330b774d1aeacd34405a35c9f0aceb245859bb
LuisX <15504222202@163.com>
2018-02-03 13:24:06 +0800

    rm build

--- .build/x86_64-apple-macosx10.10/debug/COpenSSL.build/ecp_nistp256.c.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/luisx/Desktop/GitHub/PerfectDemoProject/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/ecp_nistp256.c \
-  /Users/luisx/Desktop/GitHub/PerfectDemoProject/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/include/module.modulemap \
-  /Users/luisx/Desktop/GitHub/PerfectDemoProject/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/opensslconf.h
--- .build/x86_64-apple-macosx10.10/debug/COpenSSL.build/fips_ers.c.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/luisx/Desktop/GitHub/PerfectDemoProject/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/fips_ers.c \
-  /Users/luisx/Desktop/GitHub/PerfectDemoProject/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/include/module.modulemap \
-  /Users/luisx/Desktop/GitHub/PerfectDemoProject/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/opensslconf.h
